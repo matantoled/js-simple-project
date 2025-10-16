03230dead8c79e6010136f50a1b0321262dde479
Matan <matantoled@gmail.com>
2025-10-16 20:11:31 +0300

    minor change

--- app.js
+++ app.js
@@ -14,36 +14,39 @@ function showLoading(on) {
   }
 }
 
-async function onSubmit (e) {
+async function onSubmit(e) {
   e.preventDefault();
   showLoading(true);
 
   try {
-  const pokName = form.pname.value.trim().toLowerCase().replace(/[&<>"']/g, '');
-  if (!pokName) return;
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokName}`);
+  const queryName = form.pname.value.trim().toLowerCase().replace(/[&<>"']/g, '');
+  if (!queryName) return;
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${queryName}`);
   if (!res.ok) throw new Error(`HTTP ${res.status}`);
 
   const data = await res.json();
+  console.log((data));
+  console.log(Object.keys(data));
   
 
-  const height = data.height;
+  const height = data.height * 10;
   const pokeName = data.forms[0].name;
   const image = data.sprites.front_default;
-  const weight = data.weight;
+  const weight = (data.weight / 10).toFixed(1);
   const abilities = data.abilities.map(a => a.ability.name).join(", ");
   document.getElementById("result").innerHTML = `
+    <img src="${image}" alt="pokemon image">
+    <p>Name: ${pokeName}</p>
+    <p>Height: ${height} CM</p>
+    <p>Weight: ${weight}</p>
+    <p>Abilities: ${abilities}</p>`
 
-  <img src="${image}" alt="pokemon image">
-  <p>Name: ${pokName}</p>
-  <p>Height: ${height * 10} CM</p>
-  <p>Weight: ${weight}</p>
-  <p>Abilities: ${abilities}</p>`
   }
   catch(err) {
     document.getElementById("result").textContent = err;
   }
   finally {
     showLoading(false);
+    form.pname.focus();
   }
 }
